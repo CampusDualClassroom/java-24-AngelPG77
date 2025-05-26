009acca37100c2acd3143de47dc7da0fbf588dcc
AngelPG77 <angelpugagomez@gmail.com>
2025-05-26 13:04:36 +0200

    Ejercicio 24 Completado

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -6,12 +6,23 @@ import java.util.Queue;
 public class Exercise24 {
     public static Queue<String> createQueue() {
 
+        Queue<String> myCola = new LinkedList<>();
+        myCola.add("Smith");
+        myCola.add("Montessori");
+        myCola.add("Peralta");
+        myCola.add("House");
+        return myCola;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (queue.peek() != null){
+            String valor = queue.poll();
+            System.out.println(valor);
+        }
     }
 
+
     public static void main(String[] args) {
+    printAndEmptyQueue(createQueue());
 
     }
 
